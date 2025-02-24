badf5364d00ddd65300002cf153001046b8226c5
Ricardo Pereira <rgilpereira@gmail.com>
2025-02-24 13:46:56 +0000

    Copy src/pages/QuizDetails/components/TestTab.tsx from teacher-buddy

---
+++ src/pages/QuizDetails/components/TestTab.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TestTab() {
+  return (
+    <div className="p-4 bg-white rounded-lg">
+      <h2 className="text-xl font-semibold mb-4">hello</h2>
+    </div>
+  );
+}
